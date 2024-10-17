ee4ea0e3809d4f9c22becdddfe5ae9adae956d72
Andrew Long <111335392+idrewlong@users.noreply.github.com>
2024-10-17 17:50:43 -0500

    Initial commit

---
+++ src/lib/Project.svelte
@@ -0,0 +1,62 @@
+
+
+<script>
+        import { fade } from "svelte/transition";
+    
+        export let logo = "";
+        export let name = "";
+        export let description = "name";
+        export let stack = "";
+        export let width = "";
+        export let link = "";
+    
+        let urlPopup = false;
+    
+        function toggleUrlPopup(){
+            urlPopup = !urlPopup;
+        }
+</script>
+
+<div class="line"></div>
+
+<!-- //  insert projects here  -->
+<div class="relative flex flex-col gap-4 py-6 px-0">
+    <img src={logo} alt="" width={width}/>
+    <div class="mt-4">
+        {#if urlPopup}
+            <div class="url" transition:fade>
+                {link}
+            </div>
+        {/if}
+        <a class="text-xl font-bold" href={link} target="_blank" on:mouseenter={() => toggleUrlPopup()} on:mouseleave={ () => toggleUrlPopup() }>{name}</a>
+    </div>
+    <p class="text-md">
+        {description}
+    </p>
+
+    <div class="flex items-center gap-4 mt-4">
+        <!-- replace img with language or brand logo (website) of choice -->
+        <img src="/stack.svg" alt="" width="24px"/>
+        <p class="text-md">{stack}</p>
+    </div>
+</div>
+
+<div class="line"></div>
+
+<style>
+    
+        .url {
+            position: absolute;
+            border-radius: 8px;
+            top: 70px;
+            background: rgba(0, 0, 0, 0.5);
+            padding: 6px 8px;
+            left: -30px;
+        }
+        
+    
+        .line {
+            background: #2f2f2f;
+            height: 1px;
+        }
+</style>
---
+++ src/lib/Skills.svelte
@@ -0,0 +1,33 @@
+<script>
+    let pfp = '/blankpfp.webp'
+</script>
+
+
+<section class="max-w-5xl mx-auto rounded-md shadow-lg p-5 my-8 grid grid-cols-1 md:grid-cols-2 gap-5 bg-neutral-600 " id="about">
+
+    <!-- Image Div -->
+    <div><img src={pfp} alt="pfp" class="rounded-xl w-auto">
+    </div>
+
+    <div class="md:text-left text-center">
+            <div class="my-5">
+                    <h2 class="text-3xl font-bold mb-2">[Name Here]</h2>
+            </div>
+            <!-- Info #2 -->
+            <div class="my-5">
+                    <h3 class="text-xl font-bold mb-2"> 💻 Frameworks</h3>
+                    <p class="text-gray-50">[List of Frameworks]
+                    </p>
+            </div>
+            <!-- Info #3 -->
+            <div class="my-5">
+                    <h3 class="text-xl font-bold mb-2">💻 Databases</h3>
+                    <p class="text-gray-50">[List of Databases]</p>
+            </div>
+            <!-- Info #4 -->
+            <div class="my-5">
+                    <h3 class="text-xl font-bold mb-2">💻 Langauges</h3>
+                    <p class="text-gray-50">[List of Languages]</p>
+            </div>
+    </div>
+</section>
